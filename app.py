5215f7bbd62afd4891eb8781d9a8f0e278483648
chdonahue <donahue.chris@gmail.com>
2020-12-28 15:29:56 -0800

    port binding problem

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def about():
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
-   	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)
   # app.run(port=33507)
